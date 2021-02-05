7b7eff083c198abaaf8c79137d0c751ead63b3cb
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-02-05 11:55:02 +0300

    feat: inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, World');
+
+
+//Скрыть placeholder при клике
+$(document).ready(function () {
+	$('input').focus(function () {
+		$(this).data('placeholder', $(this).attr('placeholder'))
+		$(this).attr('placeholder', '');
+	});
+	$('input').blur(function () {
+		$(this).attr('placeholder', $(this).data('placeholder'));
+	});
+});
+
+//=======================================================================================>
